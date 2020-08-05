f40e3b16b3aa2d38666c618e53c4ecfde6cf65d9
HXHGTS <38959826+HXHGTS@users.noreply.github.com>
2020-08-05 15:25:38 +0800

    Add files via upload

--- ss.c
+++ ss.c
@@ -108,7 +108,9 @@ int RunServer() {
         system("echo \"/usr/bin/ssserver -c /root/ss.conf -d start\" >> /etc/rc.d/rc.local");
         system("chmod +x /etc/rc.d/rc.local");
         printf("将服务器DNS修改为谷歌DNS. . .\n");
-        system("echo \"nameserver 8.8.8.8\" > /etc/resolv.conf");
+        system("echo \"DNS1=8.8.8.8\" >> /etc/sysconfig/network-scripts/ifcfg-eth0");
+        system("echo \"DNS2=8.8.4.4\" >> /etc/sysconfig/network-scripts/ifcfg-eth0");
+        system("echo \"nameserver 8.8.8.8\" >> /etc/resolv.conf");
         system("echo \"nameserver 8.8.4.4\" >> /etc/resolv.conf");
         printf("正在重启服务器以应用配置. . .\n");
         system("reboot");
