58b2650131186dd60cc2a1d4736364f0eeb444d4
HXHGTS <38959826+HXHGTS@users.noreply.github.com>
2020-08-07 14:35:38 +0800

    Update ss.c

--- ss.c
+++ ss.c
@@ -39,8 +39,7 @@ int CreateServer() {
     system("pip3 install --upgrade pip");
     system("pip3 install greenlet");
     system("pip3 install gevent");
-    system("pip3 install shadowsocks");
-    system("pip3 install https://github.com/shadowsocks/shadowsocks/archive/master.zip -U");
+    system("sudo pip3 install https://github.com/shadowsocks/shadowsocks/archive/master.zip");
     printf("Shadowsocks服务器搭建完成!\n");
     return 0;
 }
